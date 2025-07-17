1ec6919dc712e7c6576cac24abbae57f897a0f05
ayuarista <aristaadewi8@gmail.com>
2025-07-17 21:45:15 +0800

    refactor ScrollingText component to use dynamic rendering for repeated elements

--- src/components/molecules/ScrollingText.jsx
+++ src/components/molecules/ScrollingText.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useRef } from 'react';
 import { FaRecycle } from 'react-icons/fa'; 
+import React from 'react';
 
 const ScrollingText = () => {
   const scrollRef = useRef(null);
 
+  const repeatCount = 30
   useEffect(() => {
     const scroll = scrollRef.current;
     let scrollAmount = 0;
@@ -23,62 +25,12 @@ const ScrollingText = () => {
   return (
     <div className="bg-black w-full overflow-hidden py-4 font-syne font-bold">
       <div ref={scrollRef} className="whitespace-nowrap flex items-center space-x-4">
-        <FaRecycle className="text-white" />
-        <span className="text-white text-xl">My Skills!</span>
-        <FaRecycle className="text-white" />
-        <span className="text-white text-xl">My Skills!</span>
-        <FaRecycle className="text-white" />
-        <span className="text-white text-xl">My Skills!</span>
-        <FaRecycle className="text-white" />
-        <span className="text-white text-xl">My Skills!</span>
-        <FaRecycle className="text-white" />
-        <span className="text-white text-xl">My Skills!</span>
-        <FaRecycle className="text-white" />
-        <span className="text-white text-xl">My Skills!</span>
-        <FaRecycle className="text-white" />
-        <span className="text-white text-xl">My Skills!</span>
-        <FaRecycle className="text-white" />
-        <span className="text-white text-xl">My Skills!</span>
-        <FaRecycle className="text-white" />
-        <span className="text-white text-xl">My Skills!</span>
-        <FaRecycle className="text-white" />
-        <span className="text-white text-xl">My Skills!</span>
-        <FaRecycle className="text-white" />
-        <span className="text-white text-xl">My Skills!</span>
-        <FaRecycle className="text-white" />
-        <span className="text-white text-xl">My Skills!</span>
-        <FaRecycle className="text-white" />
-        <span className="text-white text-xl">My Skills!</span>
-        <FaRecycle className="text-white" />
-        <span className="text-white text-xl">My Skills!</span>
-        <FaRecycle className="text-white" />
-        <span className="text-white text-xl">My Skills!</span>
-        <FaRecycle className="text-white" />
-        <span className="text-white text-xl">My Skills!</span>
-        <FaRecycle className="text-white" />
-        <span className="text-white text-xl">My Skills!</span>
-        <FaRecycle className="text-white" />
-        <span className="text-white text-xl">My Skills!</span>
-        <FaRecycle className="text-white" />
-        <span className="text-white text-xl">My Skills!</span>
-        <FaRecycle className="text-white" />
-        <span className="text-white text-xl">My Skills!</span>
-        <FaRecycle className="text-white" />
-        <span className="text-white text-xl">My Skills!</span>
-        <FaRecycle className="text-white" />
-        <span className="text-white text-xl">My Skills!</span>
-        <FaRecycle className="text-white" />
-        <span className="text-white text-xl">My Skills!</span>
-        <FaRecycle className="text-white" />
-        <span className="text-white text-xl">My Skills!</span>
-        <FaRecycle className="text-white" />
-        <span className="text-white text-xl">My Skills!</span>
-        <FaRecycle className="text-white" />
-        <span className="text-white text-xl">My Skills!</span>
-        <FaRecycle className="text-white" />
-        <span className="text-white text-xl">My Skills!</span>
-        <FaRecycle className="text-white" />
-        <span className="text-white text-xl">My Skills!</span>
+        {Array.from({length: repeatCount}).map((_, index) => (
+          <React.Fragment key={index}>
+            <FaRecycle className="text-white" />
+            <span className="text-white">My Skills!</span>
+          </React.Fragment>
+        ))}
         
       </div>
     </div>
